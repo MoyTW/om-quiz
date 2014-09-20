8951c174b15c0816a61ca551298292cbff73cb5a
Travis Moy <Travis.Moy@Staples.com>
2014-09-20 11:13:17 -0600

    It prints out the choices!
    As buttons, but hey, I don't have the docs I need to get radio buttons.

--- src/om_quiz/core.cljs
+++ src/om_quiz/core.cljs
@@ -1,25 +1,34 @@
 (ns om-quiz.core
-  (:require [om-quiz.questions :as questions]
+  (:require [om-quiz.questions :as q]
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
 (enable-console-print!)
 
-(def app-state (atom {:text questions/questions}))
+(def app-state (atom {:questions q/questions}))
 
-(defn question-view [question owner]
+(defn choice-view [choice owner]
   (reify
     om/IRender
     (render [_]
-      (dom/li nil (:question question)))))
+      (dom/div nil
+               (dom/button nil choice)))))
+
+(defn question-view [question-map owner]
+  (reify
+    om/IRender
+    (render [_]
+      (apply dom/div nil
+             (dom/h3 nil (:question question-map))
+             (om/build-all choice-view (:choices question-map))))))
 
 (defn quiz-view [app owner]
   (reify
     om/IRender
     (render [_]
       (dom/div nil
-               (dom/h2 nil "Blah!")
+               (dom/h2 nil "Quiz Header")
                (apply dom/ul nil
-                      (om/build-all question-view (:text app)))))))
+                      (om/build-all question-view (:questions app)))))))
 
 (om/root quiz-view app-state {:target (. js/document (getElementById "app"))})
